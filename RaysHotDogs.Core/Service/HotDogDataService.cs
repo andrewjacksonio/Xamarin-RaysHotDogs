2e1a0e99b9a03d3119bdb8162287b159c0a4774d
Lucas Araujo <lucas.lra@gmail.com>
2016-04-05 06:37:35 -0300

    Work in Progress

--- RaysHotDogs.Core/Service/HotDogDataService.cs
+++ RaysHotDogs.Core/Service/HotDogDataService.cs
@@ -11,7 +11,6 @@ namespace RaysHotDogs.Core.Service
     public static List<HotDog> GetAllHotDogs() => hotDogRepository.GetAllHotDogs();
     public static List<HotDogGroup> GetGroupedHotDogs() => hotDogRepository.GetGroupedHotDogs();
     public static List<HotDog> GetHotDogsForGroup(int hotDogsGroupId) => hotDogRepository.GetHotDogsForGroup(hotDogsGroupId);
-
     public static List<HotDog> GetFavoriteHotDogs() => hotDogRepository.GetFavoriteHotDogs();
     public static HotDog GetHotDogById(int hotDogId) => hotDogRepository.GetHotDogById(hotDogId);
   }
